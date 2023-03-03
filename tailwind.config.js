6667288eb910a702e65fce2e82b229de9e7ccd49
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-03-03 12:12:54 +0200

    color scheme update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,22 @@ module.exports = {
   theme: {
     colors: {
       'black': '#0C1618',
-      'secondary': '#162B8C',
-      'primary': '#F5CC00',
-      'orange': '#FF900F'
+      'blue': '#162B8C',
+      'yellow': '#F5CC00',
+      'orange': '#FF900F',
+      "white": "#FFFFFF",
+      "silver": "#C0C0C0",
+      "gray": "#999999",
+      "red": "#FF0000",
+      "maroon": "#800000",
+      "olive": "#808000",
+      "lime": "#00FF00",
+      "green": "#008000",
+      "aqua": "#00FFFF",
+      "teal": "#008080",
+      "navy": "#000080",
+      "fuchsia": "#FF00FF",
+      "purple": "#800080",
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
