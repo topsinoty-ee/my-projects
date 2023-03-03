9dbde1e2f707e614f10dd27b399ce875d95b94a3
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-03-03 12:12:42 +0200

    Clean up and redesign

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ pages }) => {
     return (
-        <header className="w-full max-w-full min-w-full max-h-[10vh] h-16 min-h-[3rem] bg-secondary text-primary flex justify-between p-4">
+        <header className="w-full max-w-full min-w-full max-h-[10vh] h-16 min-h-[3rem] bg-blue text-yellow flex justify-between p-4 ">
             <div className="text-2xl">Test Page</div>
 
             <div className="w-1/6 min-w-[10rem]">
                 <ul className="flex justify-evenly items-center">
                     {pages.map((page, i) =>
                         <li key={i}>
-                            <Link to={`/${page}`} className='active:nav hover:nav'>{page}</Link>
+                            <Link to={`/${page}`} className='hover:bg-navy py-1.5 px-3 rounded-lg'>{page}</Link>
                         </li>
                     )}
-
                 </ul>
             </div>
         </header>
